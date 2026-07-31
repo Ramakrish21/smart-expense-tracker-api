a399448cbe32fe15433f051c2d1d293dc590b594
Ramakrish21 <adlaramakrishna28@gmail.com>
2026-07-31 14:15:48 +0530

    Initial commit for Smart Expense Tracker API

---
+++ src/main/java/com/diligent/smartexpensetrackerapi/SmartExpenseTrackerApiApplication.java
@@ -0,0 +1,13 @@
+package com.diligent.smartexpensetrackerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartExpenseTrackerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartExpenseTrackerApiApplication.class, args);
+	}
+
+}
